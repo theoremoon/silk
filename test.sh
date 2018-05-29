581376e9a8c7e41fddd66e85158a21a25c675297
theoldmoon0602 <theoldmoon0602@gmail.com>
2018-05-29 23:56:05 +0900

    add test

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+PROGRAM=./silk
+
+function expect {
+  a=$(mktemp)
+  $PROGRAM "$a"
+  r=$(lli "$a")
+
+  if [ "$r" != "$1" ]; then
+    echo -e "\e[31mExpected $1 but got $r\e[m"
+  else
+    echo -e "\e[32mpass $1\e[m"
+  fi
+}
+
+expect 100
