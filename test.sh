50d51c11d2d15b641cfe234de7026218e765d2b2
theoldmoon0602 <theoldmoon0602@gmail.com>
2018-06-02 19:07:16 +0900

    add test case

--- test.sh
+++ test.sh
@@ -30,8 +30,10 @@ function failed {
       exit 1
     fi
   fi
+}
 
-
+function example {
+  expect "$(cat "examples/$1")" "$2"
 }
 
 expect "def main() {print(100)}" "100"
@@ -41,6 +43,8 @@ expect "def f(arg) { arg+100 } def main() {print(f(10))}" "110"
 expect "def f(a, b) { a + b } def main() {print(f(100, 10))}" "110"
 expect "def main() { a = if 1 { 1 } else { 2 } print(a) }" "1"
 expect "def main() { a = if 0 { 1 } else { 2 } print(a) }" "2"
+
+example "factorial.silk" "120"
 
 failed "abcd"
 failed "def mainer { print(100) }"
