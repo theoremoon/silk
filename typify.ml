776a1ccc7a5ab7687cc1f84c8ad202ffdd3e733a
theoldmoon0602 <theoldmoon0602@gmail.com>
2018-07-01 18:11:51 +0900

    add typing for assign

--- typify.ml
+++ typify.ml
@@ -30,16 +30,20 @@ let typeof_stmt stmt =
   |ExpT (exp, t) -> t
   |DefunT (_, _, _, t) -> t
 
+(* typify args and check types for func a -> b -> c ... *)
 let rec typify_func func_t args =
   match func_t with
   |Fun (a, r) -> begin
+    (* split arguments *)
     match args with
     |x::xs -> begin
       let typed_arg = typify_exp x in
       let arg_t = typeof typed_arg in
+      (* check argument *)
       if a = arg_t then
         let ret_t, typed_args = typify_func r xs in
         (ret_t, typed_arg::typed_args)
+      (* or error *)
       else
         raise (SilkError ("TypeError: required "^(string_of_type a)^" but got "^(string_of_type arg_t)^"."))
     end
@@ -50,6 +54,9 @@ let rec typify_func func_t args =
 and typify_exp exp =
   match exp with
   |Int(v) -> IntT(v, Int)
+  |Assign (name, exp) ->
+      let typed_exp = typify_exp exp in
+      AssignT(name, typed_exp, typeof typed_exp)
   |Call (name, args) -> begin
     let func_t =
       match List.assoc_opt name builtin_optypes with
