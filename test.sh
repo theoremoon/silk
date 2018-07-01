79ea6644060d3d0e9b42d5c8b7b99159a66d679c
theoldmoon0602 <theoldmoon0602@gmail.com>
2018-07-01 10:49:55 +0900

    update test

--- test.sh
+++ test.sh
@@ -15,18 +15,17 @@ function shouldbe {
   if [ "$r" != "$2" ]; then
     echo -e "\e[31mExpected $2 but got $r (in case $1)\e[m"
     return 1
-  else
-    echo -e "\e[32mpass $1\e[m"
   fi
 }
 
 function example {
-  shouldbe "$(cat "examples/$1")" "$2"
+  shouldbe "$(cat "examples/$1")" "$2" && echo -e "\e[32mpass $1\e[m"
 }
 
 
 example "print1.silk" "1"
 example "arith.silk" "11"
 example "var.silk" "-20"
+example "add2.silk" "3"
 example "factorial.silk" "120"
 
